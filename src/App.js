f7deda76968d9eea643e52bc4e3c46b488bb53fe
Sunny Mishra <sunnymishra9007689708@gmail.com>
2024-03-02 02:47:14 +0530

    The initial home page is made which will be shown when the site loads for the first time

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import Home from './Routes/Home/home.component';
 import BMI from './Routes/BMI/bmi.component';
 import SimpleInterest from './Routes/SimpleInterest/simpleInterest.component';
 import Monster from './Routes/MonstersClub/monster.component';
+import InitialHome from './Routes/InitialHome/initialhome.component';
 function App() {
   return (
     <div>
       <Routes>
         <Route path='/' element={<Home/>}>
+          <Route index element={<InitialHome/>}></Route>
           <Route path='/bmi-calculator' element={<BMI/>}/>
           <Route path='/simple-interest' element={<SimpleInterest/>}/>
           <Route path='/monster-club' element={<Monster/>}/>
---
+++ src/Routes/InitialHome/initialhome.component.jsx
@@ -0,0 +1,16 @@
+
+import './initialhome.style.css';
+
+const InitialHome = ()=>{
+    return(
+        <div className="initial-home">
+            <h1>
+                Click on a project,
+            </h1>
+            <h1>
+            Don't worry it won't Blast 💥
+            </h1>
+        </div>
+    )
+}
+export default InitialHome;
